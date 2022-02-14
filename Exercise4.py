bc979e05bce78b17f0e3cf904c8ef85c2fe2a680
Haak, R. (Romeo) <r.haak@student.ru.nl>
2022-02-14 22:54:26 +0100

    Added comments to exercise 4. Updated exercise 6 with comments, included tournament selection and swapped distance to fitness in the plots.

--- Exercise4.py
+++ Exercise4.py
@@ -4,13 +4,17 @@ import matplotlib.pyplot as plt
 
 
 def GA(mode):
+    # Pick a random number between 2^100 and 0. If needed adjust to be l bits long by padding 0 on left
     x = bin(random.randint(0, 2**l))[2:].rjust(l, '0')
     fitness = []
     for m in range(n_iterations):
+        # Create array indicating which indices flip
         flips = np.random.choice([0, 1], l, p=[1-p, p])
+        # flip bits by using xor or remain the same when they don't need to be flipped
         x_m = ''.join(format(int(bit) ^ flip, 'b') if flip else bit
                       for bit, flip in zip(x, flips))
 
+        # Depending on which mode, check score and replace (b) or just replace (c)
         if mode == 'b':
             score = x_m.count('1')
             if score > x.count('1'):
@@ -19,6 +23,7 @@ def GA(mode):
             x = x_m
         else:
             print('invalid mode')
+        # Save fitness (counting the 1s present) score
         fitness.append(x.count('1'))
     return fitness
 
